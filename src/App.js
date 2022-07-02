c98274f5e165031eead975aea0350e92f0229f93
ashrafulislampro <ashrafulislam1659@gmail.com>
2022-07-02 22:44:44 +0600

    successfully git pull

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import './App.css';
-import 'react-toastify/dist/ReactToastify.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import About from './Pages/About/About';
-import Contact from './Pages/Contact/Contact';
-import Tour from './Pages/Tour/Tour';
-import Hotel from './Pages/Hotel/Hotel';
-import NotFound from './Pages/NotFound/NotFound';
-import Signin from './Pages/Signin/Signin';
-import Signup from './Pages/Signup/Signup';
-import { ToastContainer } from 'react-toastify';
-import RequireAuth from './Components/RequireAuth';
-import Footer from './Pages/Footer/Footer';
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+// import RequireAuth from './Components/RequireAuth';
+import Footer from "./Pages/Footer/Footer";
+import Home from "./Pages/Home/Home";
+import Hotel from "./Pages/Hotel/Hotel";
+import NotFound from "./Pages/NotFound/NotFound";
+import Signin from "./Pages/Signin/Signin";
+import Signup from "./Pages/Signup/Signup";
+import Tour from "./Pages/Tour/Tour";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/tour' element={<Tour/>} />
-        <Route path='/hotel' element={<Hotel/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/signin' element={<Signin/>} />
-        <Route path='/signup' element={<Signup/>} />
-        <Route path='*' element={<NotFound/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/tour" element={<Tour />} />
+        <Route path="/hotel" element={<Hotel />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer/>
-      <ToastContainer/>
+      <Footer />
+      <ToastContainer />
     </div>
   );
 }
