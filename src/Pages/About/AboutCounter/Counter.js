35a84d0987cc06907d45a5e13226dad37c850303
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-07-02 23:06:45 +0600

    added about animation

--- src/Pages/About/AboutCounter/Counter.js
+++ src/Pages/About/AboutCounter/Counter.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Success } from '../../../Components/Data';
 import CounterDetails from './CounterDetails';
+import Swing from 'react-reveal/Swing';
 
 const Counter = () => {
     return (
         <div className="">
-            <h3 className="text-5xl font-semibold font-serif">Our Success Story</h3>
+            <Swing><h3 className="text-5xl font-semibold font-serif">Our Success Story</h3></Swing>
             <div className='py-20 bg-white px-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 '>
             {
                 Success.map((data, index) => <CounterDetails data={data} key={index} />)
