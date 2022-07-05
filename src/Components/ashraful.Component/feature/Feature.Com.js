06eda8c43a8e4b7e25f5efdf1f7a7ddda173bfdc
ashrafulislampro <ashrafulislam1659@gmail.com>
2022-07-05 22:51:19 +0600

    tour and hotel page design and functionality updated

--- src/Components/ashraful.Component/feature/Feature.Com.js
+++ src/Components/ashraful.Component/feature/Feature.Com.js
@@ -1,19 +1,24 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import featureData from "../../../Services/featureData.json";
 import ElasticSlider from "../AllCarouselSlider/ElasticSlider";
 
 const Feature = () => {
   return (
-    <div className="container px-8 mx-auto py-44">
-      <div className="mb-10">
-        <h5 className="text-lg">Featured</h5>
-        <h2 className="text-3xl py-5 mb-7">
-          <span className="font-bold">Featured </span>Destination
-        </h2>
-      </div>
-      <div className="mt-10">
-        <ElasticSlider featureData={featureData} />
-      </div>
+    <div className="container px-8 mx-auto py-16">
+      <Fade left>
+        <div>
+          <h5 className="text-lg">Featured</h5>
+          <h2 className="text-3xl">
+            <span className="font-bold">Featured </span>Destination
+          </h2>
+        </div>
+      </Fade>
+      <Fade bottom>
+        <div className="">
+          <ElasticSlider featureData={featureData} />
+        </div>
+      </Fade>
     </div>
   );
 };
